2dfe875e68574d912a532451ca78d10198a181c5
joaquin <brujoaco@gmail.com>
2024-01-31 18:20:35 -0300

    empezando css

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Sorry, page not found</p>
+    </div>
+  )
+}
+
+export default NotFound
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,12 @@
-import Link from "next/link";
+import Links from "./links/Links";
+import styles from './navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>Logo</div>
       <div>
-        <Link href="/">Homepage</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
+        <Links />
       </div>
     </div>
   );
